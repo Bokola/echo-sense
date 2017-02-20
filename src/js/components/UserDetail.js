ba58107fce58e6c39ae64bc52a02065d80f6ea6d
Jeremy Gordon <onejgordon@gmail.com>
2017-02-20 16:59:38 +0300

    UI to test GCM messages

--- src/js/components/UserDetail.js
+++ src/js/components/UserDetail.js
@@ -4,24 +4,26 @@ var $ = require('jquery');
 var DialogChooser = require('components/DialogChooser');
 var LoadStatus = require('components/LoadStatus');
 var AppConstants = require('constants/AppConstants');
-var mui = require('material-ui');
-var RefreshIndicator = mui.RefreshIndicator;
-var RaisedButton = mui.RaisedButton;
-var FlatButton = mui.FlatButton;
-var IconButton = mui.IconButton;
+import {RaisedButton, TextField, FlatButton, RefreshIndicator,
+  IconButton} from 'material-ui';
 var util = require('utils/util');
 var toastr = require('toastr');
 var bootbox = require('bootbox');
 import {browserHistory} from 'react-router';
-
+import {changeHandler} from 'utils/component-utils';
 var Link = Router.Link;
+var api = require('utils/api');
 
+@changeHandler
 export default class UserDetail extends React.Component {
   static defaultProps = {  };
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      gcm_form: {
+
+      }
     };
   }
 
@@ -58,12 +60,34 @@ export default class UserDetail extends React.Component {
     }
   }
 
+  send_gcm_test() {
+    let {gcm_form, user} = this.state;
+    if (gcm_form.message) {
+      let params = {
+        user_ids: user.id,
+        message: gcm_form.message
+      }
+      api.post("/admin/actions/gcm/manual", params, (res) => {
+
+      });
+    }
+  }
+
   render() {
     var u = this.state.user;
     var content;
+    let _gcm_test;
     if (!u) {
       content = (<RefreshIndicator size={40} left={50} top={50} status="loading" />);
     } else {
+      let {gcm_form} = this.state;
+      if (u.gcm_reg_id) _gcm_test = (
+        <div>
+          <TextField placeholder="Message (JSON)" onChange={this.changeHandler.bind(this, 'gcm_form', 'message')} value={gcm_form.message} />
+          <RaisedButton label="Send GCM Message" primary={true} onClick={this.send_gcm_test.bind(this)} />
+          <p className="help-text"><small>Payload example: { JSON.stringify({"message": "hello"}) }</small></p>
+        </div>
+        );
       content = (
         <div>
           <h1>User - { u.id }</h1>
@@ -73,6 +97,8 @@ export default class UserDetail extends React.Component {
             <b>Phone:</b> <span>{ u.phone || "" }</span><br/>
             <b>Created:</b> <span>{ util.printDate(u.ts_created, true) }</span><br/>
             <b>GCM Reg ID:</b> <span>{ u.gcm_reg_id }</span>
+
+            { _gcm_test }
 
           </div>
 
